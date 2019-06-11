816df7fe033d4da47fd81c98d19e694f82f2129d
김현규 <djdu1018@gmail.com>
2019-06-11 18:56:15 +0900

    LuaCFunctionClosure added. Not testing..

--- Dalbit/Dalbit.h
+++ Dalbit/Dalbit.h
@@ -1,8 +1,33 @@
 #pragma once
+
+#define GETVAR(T,I,V) *(T*)V[I*BLOCKSIZE*UNITSIZE]
+
+#include <functional>
+
 struct lua_State;
+
+
+#include "../Lua53/lua.hpp"
+
 
 namespace Dalbit
 {
 	void CallLuaFunction(lua_State* L, const char* func, const char* argSig, int resNum, ...);
 	void StackDump(lua_State* L);
+
+
+	class LuaCFunctionClosure
+	{
+	public:
+		static const int BLOCKSIZE = sizeof(double);
+		static const int UNITSIZE = sizeof(char);
+		static const int POOLSIZE = 8;
+
+	private:
+		unsigned char varMemPool[BLOCKSIZE*UNITSIZE*POOLSIZE];
+		std::function<int(lua_State*)> closureFunction;
+
+	public:
+		lua_CFunction MakeClosure(const char* argSig, double(*fp)(void** vars));
+	};
 }
