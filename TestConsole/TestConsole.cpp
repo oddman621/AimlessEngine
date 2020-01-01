aba7d00d13755c89bda747a578922f02d139c570
oddman621 <oddman621@gmail.com>
2020-01-01 20:38:44 +0900

    첫번째 방법 최적화

--- TestConsole/TestConsole.cpp
+++ TestConsole/TestConsole.cpp
@@ -38,16 +38,16 @@ int main()
         return -2;
     }
 
-    const GLfloat red[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    glClearColor(0.0f, 0.4f, 0.0f, 1.0f);               // First Method
+    const GLfloat red[4] = { 1.0f, 0.0f, 0.0f, 1.0f };  // Second Method
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         // First Method
-        //glClearColor(0.0f, 0.4f, 0.0f, 1.0f);
-        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         // Second Method
-        glClearBufferfv(GL_COLOR, 0, red);
+        //glClearBufferfv(GL_COLOR, 0, red);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
